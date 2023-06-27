f795f63bf73caf61af1da31d3c8c5b6fc2b15659
Xhovani Mali <xhovani03@live.com>
2023-06-27 07:42:24 -0400

    updated pages and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React from "react";
+// import components
+import Socials from "./Socials";
+import Logo from "../img/header/logo.svg";
+import MobileNav from "./MobileNav";
+
+//import link
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header
+      className="bg-pink-200 fixed w-full px-[30px]
+  lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center"
+    >
+      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        {/* Logo */}
+        <Link to={"/"}>
+          <img src={Logo} style={{ width: 400 }} alt="" />
+        </Link>
+
+        {/* Nav - initially hidden - show on desktop mode */}
+        <nav className="hidden lg:flex gap-x-12 font-semibold">
+          <Link
+            to={"/Home"}
+            className="text-[#697c6d] 
+      hover:text-primary transistion"
+          >
+            Home
+          </Link>
+        </nav>
+        <nav className="hidden lg:flex">
+          <Link
+            to={"/About"}
+            className="text-[#697c6d] 
+      hover:text-primary transistion"
+          >
+            About
+          </Link>
+        </nav>
+        <nav className="hidden lg:flex">
+          <Link
+            to={"/Portfolio"}
+            className="text-[#697c6d] 
+      hover:text-primary transistion"
+          >
+            Portfolio
+          </Link>
+        </nav>
+        <nav className="hidden lg:flex">
+          <Link
+            to={"/Contact"}
+            className="text-[#697c6d] 
+      hover:text-primary transistion"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+    return <section className="section bg-yellow-200">About
+    </section>;
 };
 
 export default About;
